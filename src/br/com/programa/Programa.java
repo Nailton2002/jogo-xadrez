ff7b89c93f52f2d096da52b0943d11697466d4b1
Nailton2002 <jnmp25jnmp@gmail.com>
2022-08-27 20:12:27 -0300

    Primeira classe de posicao

--- src/br/com/programa/Programa.java
+++ src/br/com/programa/Programa.java
@@ -1,10 +1,13 @@
 package br.com.programa;
 
+import br.com.tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Posicao posicao = new Posicao(3, 5);
+		System.out.println(posicao);
 	}
 
 }
